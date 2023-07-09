f300b3c66dfe9ac4cde0037bd10f0f6e47607706
Igor Sidorov <igor.sidorov@binarycode.ru>
2023-07-09 18:08:26 +0300

    integration test

--- flake.nix
+++ flake.nix
@@ -6,10 +6,24 @@
 
   outputs = inputs: let
     module = import ./module.nix;
+
+    pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
   in {
     formatter = {
       aarch64-darwin = inputs.nixpkgs-darwin.legacyPackages.aarch64-darwin.alejandra;
-      x86_64-linux = inputs.nixpkgs.legacyPackages.x86_64-linux.alejandra;
+      x86_64-linux = pkgs.alejandra;
+    };
+
+    checks.x86_64-linux.integration = pkgs.nixosTest {
+      nodes.machine = _: {
+        imports = [ inputs.self.nixosModules.default ];
+      };
+
+      testScript = ''
+        start_all()
+
+        machine.wait_for_unit("multi-user.target")
+      '';
     };
 
     nixosModules.default = module;
