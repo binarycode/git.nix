f931bf525dd1e0dad58f3035ea50b421e5f9fd4c
Igor Sidorov <igor.sidorov@binarycode.ru>
2023-07-09 15:55:49 +0300

    add nix formatter

--- flake.nix
+++ flake.nix
@@ -1,14 +1,18 @@
 {
-  outputs = _: let
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixos-23.05;
+    nixpkgs-darwin.url = github:nixos/nixpkgs/nixpkgs-23.05-darwin;
+  };
+
+  outputs = inputs: let
     module = import ./module.nix;
   in {
-    nixosModules = {
-      default = module;
-      git     = module;
+    formatter = {
+      aarch64-darwin = inputs.nixpkgs-darwin.legacyPackages.aarch64-darwin.alejandra;
+      x86_64-linux = inputs.nixpkgs.legacyPackages.x86_64-linux.alejandra;
     };
-    darwinModules = {
-      default = module;
-      git     = module;
-    };
+
+    nixosModules.default = module;
+    darwinModules.default = module;
   };
 }
